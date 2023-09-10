18c76afce9e9a44dcb23e7d437c5827236f625c0
colby <colbykrawczyk10@gmail.com>
2023-09-10 12:49:26 -0400

    Add bStats

--- src/main/java/com/github/firewolf8385/showitem/ShowItemPlugin.java
+++ src/main/java/com/github/firewolf8385/showitem/ShowItemPlugin.java
@@ -1,17 +1,14 @@
 package com.github.firewolf8385.showitem;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ShowItemPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        // Enables bStats for statistics tracking.
+        new Metrics(this, 19781);
     }
 }
