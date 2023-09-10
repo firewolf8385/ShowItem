e38da455957a48f047851819a0c0406ad31c43e4
colby <colbykrawczyk10@gmail.com>
2023-09-10 12:25:44 -0400

    Initial commit

---
+++ src/main/java/com/github/firewolf8385/showitem/ShowItemPlugin.java
@@ -0,0 +1,17 @@
+package com.github.firewolf8385.showitem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShowItemPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
